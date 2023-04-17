47592e02db2ed9309811f06d64f3e41eee3a5833
Alden Davidson <adavidson+github@protonmail.ch>
2023-04-17 07:37:53 -0700

    Add version tag

--- git-together-ssh.rb
+++ git-together-ssh.rb
@@ -3,6 +3,7 @@ class GitTogetherSsh < Formula
 
   desc "Better commit attribution while pairing, with SSH certs!"
   homepage "https://github.com/ananaso/git-together-ssh"
+  version VERSION
   url "https://github.com/ananaso/git-together-ssh/releases/download/v#{VERSION}/git-together-ssh-v#{VERSION}-x86_64-apple-darwin.tar.gz"
   sha256 "4d4bc2385f4e7f129a4ddd72bf3ea7fb11571e551c92efb7a7a53bbb2f84c4f7"
   license "MIT"
