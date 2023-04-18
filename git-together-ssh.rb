bf78b624021988898a18b70e73465c2e6e50d4db
Alden Davidson <adavidson+github@protonmail.ch>
2023-04-18 07:36:28 -0700

    Bump to version 0.1.1

--- git-together-ssh.rb
+++ git-together-ssh.rb
@@ -1,11 +1,11 @@
 class GitTogetherSsh < Formula
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 
   desc "Better commit attribution while pairing, with SSH certs!"
   homepage "https://github.com/ananaso/git-together-ssh"
   version VERSION
   url "https://github.com/ananaso/git-together-ssh/releases/download/v#{VERSION}/git-together-ssh-v#{VERSION}-x86_64-apple-darwin.tar.gz"
-  sha256 "6ecae234b61ab84523ca049850ad241f9ba4beb4500430102d51879f94c48942"
+  sha256 "b63e8e6ab4f3d193cab49ea7ed86b9daf5ab81086588fd74583c7684669dd23b"
   license "MIT"
 
   depends_on "libssh2"
