25d34b63d18a194e347700f20f31b015fdaacc9f
Alden Davidson <adavidson+github@protonmail.ch>
2023-08-03 14:32:27 -0700

    Create git-together.rb
    This homebrew file will allow installing of the non-ssh version just released

---
+++ git-together.rb
@@ -0,0 +1,21 @@
+class GitTogether < Formula
+  VERSION = "0.1.2"
+
+  desc "Better commit attribution while pairing!"
+  homepage "https://github.com/ananaso/git-together-ssh"
+  version VERSION
+  url "https://github.com/ananaso/git-together-ssh/releases/download/v#{VERSION}/git-together-ssh-v#{VERSION}-x86_64-apple-darwin.tar.gz"
+  sha256 "b63e8e6ab4f3d193cab49ea7ed86b9daf5ab81086588fd74583c7684669dd23b"
+  license "MIT"
+
+  depends_on "libssh2"
+  depends_on "openssl@1.1"
+  
+  def install
+    bin.install "git-together"
+  end
+
+  test do
+    system "#{bin}/git-together"
+  end
+end
