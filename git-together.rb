965950cf2c0afc6537d14aab17711d3638a6ea1f
Alden Davidson <adavidson+github@protonmail.ch>
2023-08-03 14:47:29 -0700

    Fix tar.gz filename

--- git-together.rb
+++ git-together.rb
@@ -4,7 +4,7 @@ class GitTogether < Formula
   desc "Better commit attribution while pairing!"
   homepage "https://github.com/ananaso/git-together-ssh"
   version VERSION
-  url "https://github.com/ananaso/git-together-ssh/releases/download/v#{VERSION}/git-together-ssh-v#{VERSION}-x86_64-apple-darwin.tar.gz"
+  url "https://github.com/ananaso/git-together-ssh/releases/download/v#{VERSION}/git-together-v#{VERSION}-x86_64-apple-darwin.tar.gz"
   sha256 "c6ce00f253b274f60e7b6fdc31dfc13ec18bd8695e6e5a6872a7f792b783e368"
   license "MIT"
 
