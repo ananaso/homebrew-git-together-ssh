5d28ca06f90f90f10898a0b22d9dc1783be56036
Alden Davidson <adavidson+github@protonmail.ch>
2023-08-22 10:39:30 -0700

    Update git-together sha256

--- git-together.rb
+++ git-together.rb
@@ -5,7 +5,7 @@ class GitTogether < Formula
   homepage "https://github.com/ananaso/git-together-ssh"
   version VERSION
   url "https://github.com/ananaso/git-together-ssh/releases/download/v#{VERSION}/git-together-v#{VERSION}-x86_64-apple-darwin.tar.gz"
-  sha256 "c6ce00f253b274f60e7b6fdc31dfc13ec18bd8695e6e5a6872a7f792b783e368"
+  sha256 "c620049e1f2ef611f47a53dc3ed204dcb79a0fabf1b5d4d9c628ebaa4706a8a1"
   license "MIT"
 
   depends_on "libssh2"
